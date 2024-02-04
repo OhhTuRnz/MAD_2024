95d25499881ab4be2bdb602f967151be88d2486b
OhhTuRnz <alejandro.carrasco.aragon@alumnos.upm.es>
2024-02-04 12:34:57 +0100

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_2024_App"
+include(":app")
+ 
