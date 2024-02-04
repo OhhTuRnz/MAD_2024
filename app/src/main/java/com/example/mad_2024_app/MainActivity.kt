dd2faf41076d7be0f4f63632cc81cb94ac7bb602
OhhTuRnz <alejandro.carrasco.aragon@alumnos.upm.es>
2024-02-04 13:10:27 +0100

    Hello World functionality. Home Page.

--- app/src/main/java/com/example/mad_2024_app/MainActivity.kt
+++ app/src/main/java/com/example/mad_2024_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_2024_app
 
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,26 +13,69 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mad_2024_app.ui.theme.MAD_2024_AppTheme
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+
+val PurpleColor = Color(0xFF6200EE)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MAD_2024_AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MyAppContent()
             }
         }
     }
 }
 
 @Composable
+fun MyAppContent() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.LightGray
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Header(headerColor = PurpleColor)
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Greeting(name = "Android")
+    }
+}
+
+@Composable
+fun Header(headerColor: Color) {
+    Surface(
+        color = headerColor,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "ArcgisKt",
+            color = Color.White,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
             text = "Hello $name!",
-            modifier = modifier
-    )
+            modifier = modifier,
+        )
+    }
 }
 
 @Preview(showBackground = true)
